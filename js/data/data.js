15f96e752ddf9b3d528807890fc676177213331f
Kevin Nguyen <account@kevinwin.com>
2016-04-03 14:51:18 -0700

    Add photo data

---
+++ js/data/data.js
@@ -0,0 +1,27 @@
+var imageData = [
+  {
+    title: 'Einstein',
+    url: 'http://static1.businessinsider.com/image/5252ededeab8eabb6446cc0b/12-famous-quotes-that-always-get-misattributed.jpg',
+    rating: 0
+  },
+  {
+    title: 'Saigon execution',
+    url: 'http://monovisions.com/wp-content/uploads/2015/04/10-famous-black-and-white-photojournalists-Eddie-Adams.jpg',
+    rating: 0
+  },
+  {
+    title: 'Flag Raising at Iwo Jima',
+    url: 'http://cdn.history.com/sites/2/2015/03/lists-7-famous-photos-iwo-jima-flag-raising-xl-E.jpeg',
+    rating: 0
+  },
+  {
+    title: 'VJ Day in Times Square',
+    url: 'http://static1.businessinsider.com/image/5252ededeab8eabb6446cc0b/12-famous-quotes-that-always-get-misattributed.jpg',
+    rating: 0
+  },
+  {
+    title: 'Ali',
+    url: 'http://framingpainting.com/UploadPic/Unknown%20Artist/big/Muhammad%20Ali%20vs.%20Sonny%20Liston.jpg',
+    rating: 0
+  }
+];
