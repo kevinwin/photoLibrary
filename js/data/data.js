98346855a6c282c267b4a1b29866e0824198eb1d
Kevin Nguyen <account@kevinwin.com>
2016-04-03 20:39:52 -0700

    Update data

--- js/data/data.js
+++ js/data/data.js
@@ -10,13 +10,13 @@ var imageData = [
     rating: 0
   },
   {
-    title: 'Flag Raising at Iwo Jima',
+    title: 'Iwo Jima',
     url: 'http://cdn.history.com/sites/2/2015/03/lists-7-famous-photos-iwo-jima-flag-raising-xl-E.jpeg',
     rating: 0
   },
   {
-    title: 'VJ Day in Times Square',
-    url: 'http://static1.businessinsider.com/image/5252ededeab8eabb6446cc0b/12-famous-quotes-that-always-get-misattributed.jpg',
+    title: 'VJ Day',
+    url: 'http://i0.wp.com/listverse.com/wp-content/uploads/2014/08/Legendary_kiss_V%E2%80%93J_day_in_Times_Square_Alfred_Eisenstaedt.jpg?resize=632%2C420',
     rating: 0
   },
   {
